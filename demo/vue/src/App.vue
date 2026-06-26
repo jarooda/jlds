aa09ef474118f759950a2e52cebcb28921c164d1
Jalu Wibowo <jaluwibowoaji@gmail.com>
2026-06-27 00:48:53 +0700

    chore: adjust demo

--- demo/vue/src/App.vue
+++ demo/vue/src/App.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from './components/ui/button';
+
+function test () {
+  alert('Hello World')
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <Button @click="test">
+    Click Me
+  </Button>
 </template>
 
 <style scoped></style>
